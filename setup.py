d246296b58952c677bc7b666a6ef2c0533380e43
Akson <aksonsam@gmail.com>
2024-10-20 19:42:02 +0000

    minor change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gesund',
-    version='0.3.17',
+    version='0.3.18',
     author='Gesund AI',
     author_email='veysel@gesund.ai',
     license="MIT",
