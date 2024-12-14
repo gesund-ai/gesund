refactor(core): fixed lift chart metric calculation

gesund/core/_metrics/common/lift_chart.py
@@ -5,22 +5,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+import seaborn as sns
 
 from gesund.core import metric_manager, plot_manager
-
-COHORT_SIZE_LIMIT = 2
-DEBUG = True
-
-
-def categorize_age(age):
-    if age < 18:
-        return "Child"
-    elif 18 <= age < 30:
-        return "Young Adult"
-    elif 30 <= age < 60:
-        return "Adult"
-    else:
-        return "Senior"
 
 
 class Classification:
@@ -81,56 +68,6 @@ class Classification:
                 prediction.append(sample_pred["prediction_class"])
         return (np.asarray(prediction), np.asarray(ground_truth))
 
-    def apply_metadata(self, data: dict) -> dict:
-        """
-        Applies metadata to the data for metric calculation and plotting.
-
-        :param data: The input data required for calculation, {"prediction":, "ground_truth": , "metadata":}
-        :type data: dict
-
-        :return: Filtered dataset
-        :rtype: dict
-        """
-        # TODO:: This function could be global to be applied across metrics
-
-        df: pd.DataFrame = pd.DataFrame.from_records(data["metadata"])
-        cohorts_data = {}
-
-        metadata_columns = df.columns.tolist()
-        metadata_columns.remove("image_id")
-        lower_case = {i: i.lower() for i in metadata_columns}
-        df = df.rename(columns=lower_case)
-
-        if "age" in list(lower_case.values()):
-            df["age"] = df["age"].apply(categorize_age)
-
-        for grp, subset_data in df.groupby(list(lower_case.values())):
-            grp_str = ",".join([str(i) for i in grp])
-
-            if subset_data.shape[0] < COHORT_SIZE_LIMIT:
-                print(
-                    f"Warning - grp excluded - {grp_str} cohort size < {COHORT_SIZE_LIMIT}"
-                )
-            else:
-                image_ids = set(subset_data["image_id"].to_list())
-                filtered_data = {
-                    "prediction": {
-                        i: data["prediction"][i]
-                        for i in data["prediction"]
-                        if i in image_ids
-                    },
-                    "ground_truth": {
-                        i: data["ground_truth"][i]
-                        for i in data["ground_truth"]
-                        if i in image_ids
-                    },
-                    "metadata": subset_data,
-                    "class_mapping": data["class_mapping"],
-                }
-                cohorts_data[grp_str] = filtered_data
-
-        return data
-
     def __calculate_metrics(self, data: dict, class_mapping: dict) -> dict:
         """
         A function to calculate the metrics
@@ -145,10 +82,7 @@ class Classification:
         """
         class_order = [int(i) for i in class_mapping.keys()]
 
-        if len(class_order) > 2:
-            prediction, ground_truth = self.__preprocess(data, get_logits=True)
-        else:
-            prediction, ground_truth = self.__preprocess(data)
+        prediction, ground_truth = self.__preprocess(data, get_logits=True)
 
         lift_chart_calculator = LiftChart(class_mapping)
         lift_points = lift_chart_calculator.calculate_lift_curve_points(
@@ -168,7 +102,7 @@ class Classification:
         Calculates the lift chart for the given data.
 
         :param data: The input data required for calculation and plotting
-                     {"prediction":, "ground_truth": , "metadata":, "class_mappings":}
+                     {"prediction":, "ground_truth": , "metadata":, "class_mapping":}
         :type data: dict
 
         :return: Calculated metric results
@@ -178,27 +112,16 @@ class Classification:
 
         # Validate the data
         self._validate_data(data)
-        metadata = data.get("metadata")
 
-        if DEBUG:
-            metadata = None
-
-        if metadata:
-            cohort_data = self.apply_metadata(data)
-            for _cohort_key in cohort_data:
-                result[_cohort_key] = self.__calculate_metrics(
-                    cohort_data[_cohort_key], data.get("class_mapping")
-                )
-        else:
-            result = self.__calculate_metrics(data, data.get("class_mapping"))
-
+        # calculate the metrics
+        result = self.__calculate_metrics(data, data.get("class_mapping"))
         return result
 
 
 class LiftChart:
-    def __init__(self, class_mappings: Dict[int, str]) -> None:
-        self.class_mappings = class_mappings
-        self.class_order = [int(i) for i in list(class_mappings.keys())]
+    def __init__(self, class_mapping: Dict[int, str]) -> None:
+        self.class_mapping = class_mapping
+        self.class_order = [int(i) for i in list(class_mapping.keys())]
 
     def _decile_table(
         self,
@@ -215,7 +138,7 @@ class LiftChart:
         :return: Dataframe containing decile metrics
         """
         df = pd.DataFrame(
-            {"true": true, "pred_logits": pred_logits.loc[predicted_class]}
+            {"true": true, "pred_logits": pred_logits.loc[:, predicted_class]}
         )
 
         # Sort by predicted logits in descending order
@@ -271,18 +194,19 @@ class LiftChart:
     def calculate_lift_curve_points(
         self,
         true: Union[List[int], np.ndarray, pd.Series],
-        pred_logits: pd.DataFrame,
+        pred_logits: Union[List[int], np.ndarray, pd.Series],
         predicted_class: Optional[int] = None,
         change_deciles: int = 20,
         labels: bool = True,
         round_decimal: int = 3,
     ) -> Dict[str, List[Dict[str, float]]]:
+        pred_df = pd.DataFrame(pred_logits)
         class_lift_dict = dict()
         if predicted_class in [None, "all", "overall"]:
             for class_ in self.class_order:
                 lift_df = self._decile_table(
                     true,
-                    pred_logits,
+                    pred_df,
                     predicted_class=int(class_),
                     change_deciles=change_deciles,
                     labels=labels,
@@ -292,12 +216,12 @@ class LiftChart:
                     {"x": avg_prob, "y": lift}
                     for avg_prob, lift in zip(lift_df["prob_avg"], lift_df["lift"])
                 ]
-                class_name = self.class_mappings[class_]
+                class_name = self.class_mapping[str(class_)]
                 class_lift_dict[class_name] = xy_points
         else:
             lift_df = self._decile_table(
                 true,
-                pred_logits,
+                pred_df,
                 predicted_class=predicted_class,
                 change_deciles=change_deciles,
                 labels=labels,
@@ -307,7 +231,7 @@ class LiftChart:
                 {"x": avg_prob, "y": lift}
                 for avg_prob, lift in zip(lift_df["prob_avg"], lift_df["lift"])
             ]
-            class_name = self.class_mappings[predicted_class]
+            class_name = self.class_mapping[str(predicted_class)]
             class_lift_dict[class_name] = xy_points
 
         return class_lift_dict
@@ -322,11 +246,12 @@ class ObjectDetection:
 
 
 class PlotLiftChart:
-    def __init__(self, data: dict):
+    def __init__(self, data: dict, cohort_id: Optional[int] = None):
         self.data = data
         self.lift_points = data["lift_points"]
-        self.class_mappings = data["class_mappings"]
+        self.class_mapping = data["class_mapping"]
         self.class_order = data["class_order"]
+        self.cohort_id = cohort_id
 
     def _validate_data(self):
         """
@@ -348,7 +273,12 @@ class PlotLiftChart:
         dir_path = "plots"
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
-        filepath = f"{dir_path}/{filename}"
+
+        if self.cohort_id:
+            filepath = f"{dir_path}/{self.cohort_id}_{filename}"
+        else:
+            filepath = f"{dir_path}/{filename}"
+
         fig.savefig(filepath, format="png")
         return filepath
 
@@ -359,6 +289,7 @@ class PlotLiftChart:
         :return: Matplotlib Figure object
         :rtype: Figure
         """
+        sns.set_style("whitegrid")
         # Validate the data
         self._validate_data()
 
@@ -368,11 +299,21 @@ class PlotLiftChart:
             y = [p["y"] for p in points]
             ax.plot(x, y, marker="o", label=f"Class {class_name}")
 
-        ax.set_xlabel("Average Predicted Probability")
-        ax.set_ylabel("Lift")
-        ax.set_title("Lift Chart")
+        ax.set_xlabel(
+            "Average Predicted Probability",
+            fontdict={"fontsize": 14, "fontweight": "medium"},
+        )
+        ax.set_ylabel(
+            "Calculated Lift", fontdict={"fontsize": 14, "fontweight": "medium"}
+        )
+
+        if self.cohort_id:
+            title_str = f"Lift Chart : cohort - {self.cohort_id}"
+        else:
+            title_str = "Lift Chart"
+
+        ax.set_title(title_str, fontdict={"fontsize": 16, "fontweight": "medium"})
         ax.legend()
-        ax.grid(True)
         return fig
 
 
@@ -403,7 +344,10 @@ def calculate_lift_chart_metric(data: dict, problem_type: str):
 
 @plot_manager.register("classification.lift_chart")
 def plot_lift_chart(
-    results: dict, save_plot: bool, file_name: str = "lift_chart.png"
+    results: dict,
+    save_plot: bool,
+    file_name: str = "lift_chart.png",
+    cohort_id: Optional[int] = None,
 ) -> Union[str, None]:
     """
     A wrapper function to plot the lift chart.
@@ -414,11 +358,13 @@ def plot_lift_chart(
     :type save_plot: bool
     :param file_name: Name of the file to save the plot
     :type file_name: str
+    :param cohort_id: id of the cohort
+    :type cohort_id: int
 
     :return: None or path to the saved plot
     :rtype: Union[str, None]
     """
-    plotter = PlotLiftChart(data=results)
+    plotter = PlotLiftChart(data=results, cohort_id=cohort_id)
     fig = plotter.plot()
     if save_plot:
         return plotter.save(fig, filename=file_name)